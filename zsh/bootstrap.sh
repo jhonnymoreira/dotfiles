cc6f4f330e0f6e2a3861b7f67adc97302dcfdbf6
Jhonny Moreira <jhonnymoreira@protonmail.com>
2022-09-23 14:38:05 -0300

    add Cargo and rbenv to bootstrap

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -1,5 +1,7 @@
 BOOTSTRAP_PATH="$DOTFILES_PATH/zsh/bootstrap"
 
+. $BOOTSTRAP_PATH/cargo.sh
 . $BOOTSTRAP_PATH/ghcup.sh
 . $BOOTSTRAP_PATH/homebrew.sh
 . $BOOTSTRAP_PATH/nvm.sh
+. $BOOTSTRAP_PATH/rbenv.sh
