2ec7f3f517bb27007c93f60ac09bc87d1058a764
Jhonny Moreira <jhonnymoreira@protonmail.com>
2024-07-18 14:31:49 -0300

    chore: improve overall organization

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -6,6 +6,8 @@ BOOTSTRAP_PATH="$DOTFILES_PATH/zsh/bootstrap"
 . $BOOTSTRAP_PATH/ghcup.sh
 [[ $OSTYPE == 'darwin'* ]] && . $BOOTSTRAP_PATH/homebrew.sh
 . $BOOTSTRAP_PATH/nvm.sh
+. $BOOTSTRAP_PATH/personal-access-tokens.sh
+. $BOOTSTRAP_PATH/pnpm.sh
 . $BOOTSTRAP_PATH/pyenv.sh
 . $BOOTSTRAP_PATH/rbenv.sh
 . $BOOTSTRAP_PATH/starship.sh
