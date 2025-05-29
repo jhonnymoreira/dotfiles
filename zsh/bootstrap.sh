dc3c38c2c088aa8ac465a0cdd5d2cf4fb527b9bb
Jhonny Moreira <jhonnymoreira@protonmail.com>
2025-05-28 21:05:43 -0300

    chore: hello Deno

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -3,6 +3,7 @@ BOOTSTRAP_PATH="$DOTFILES_PATH/zsh/bootstrap"
 . $BOOTSTRAP_PATH/aws.sh
 . $BOOTSTRAP_PATH/bun.sh
 . $BOOTSTRAP_PATH/cargo.sh
+. $BOOTSTRAP_PATH/deno.sh
 . $BOOTSTRAP_PATH/ghcup.sh
 [[ $OSTYPE == 'darwin'* ]] && . $BOOTSTRAP_PATH/homebrew.sh
 . $BOOTSTRAP_PATH/lando.sh
---
+++ zsh/bootstrap/deno.sh
@@ -0,0 +1,3 @@
+. "$HOME/.deno/env"
+
+if [[ ":$FPATH:" != *":$HOME/.zsh/completions:"* ]]; then export FPATH="$HOME/.zsh/completions:$FPATH"; fi
